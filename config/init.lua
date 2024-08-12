d7be85f747e1cfdeb598a13df7b20e16d705840b
LavenderQAQ <lavenderqaq.cs@gmail.com>
2024-08-12 14:19:33 +0800

    custom: maximize window at startup
    Signed-off-by: LavenderQAQ <lavenderqaq.cs@gmail.com>

--- config/init.lua
+++ config/init.lua
@@ -1,5 +1,11 @@
 local wezterm = require('wezterm')
 
+local mux = wezterm.mux
+wezterm.on('gui-startup', function()
+    local _, _, window = mux.spawn_window{}
+    window:gui_window():maximize()
+end)
+
 ---@class Config
 ---@field options table
 local Config = {}
